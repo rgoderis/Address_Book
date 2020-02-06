4fae113aaa75c2287fdb819fc943500b2d9381f2
Ryan Goderis <rgoderis789@gmail.com>
2020-02-06 11:50:49 -0500

    created landing component that contains ContactCard component

---
+++ client/src/Components/ContactCard/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class ContactCard extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1>I am a contact</h1>
+            </div>
+        )
+    }
+}
+
+export default ContactCard
