b1eb8ba339044519f54c9c6f9d1064a023251956
Ryan Goderis <rgoderis789@gmail.com>
2020-02-06 12:09:09 -0500

    set state in Landing to contain contacts which is an array of objects for each contact info

--- client/src/Components/Landing/index.js
+++ client/src/Components/Landing/index.js
@@ -3,8 +3,48 @@ import ContactCard from "../ContactCard"
 
 class Landing extends React.Component {
     render(){
+        state = {
+            contacts: [
+                {
+                    lastName: "Goderis",
+                    firstName: "Ryan",
+                    birthday: "August 8",
+                    email: "rgoderis789@gmail.com",
+                    address: "3212 Decker Ave., Orlando FL. 32833",
+                    notes: []
+            },
+                {
+                    lastName: "Goderis",
+                    firstName: "Ryan",
+                    birthday: "August 8",
+                    email: "rgoderis789@gmail.com",
+                    address: "3212 Decker Ave., Orlando FL. 32833",
+                    notes: []
+            },
+                {
+                    lastName: "Goderis",
+                    firstName: "Ryan",
+                    birthday: "August 8",
+                    email: "rgoderis789@gmail.com",
+                    address: "3212 Decker Ave., Orlando FL. 32833",
+                    notes: []
+            },
+            ]
+        }
         return (
-            <ContactCard/>
+            <div>
+            {this.state.contacts.map(contact=>(
+                <ContactCard
+                    firstName = {contact.firstName}
+                    lastName = {contact.lastName}
+                    birthday = {contact.birthday}
+                    email = {contact.email}
+                    address = {contact.address}
+                    notes = {contact.notes}
+                />
+            ))}
+            </div>
+
         )
     }
 }
