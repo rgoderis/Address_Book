530c68d613e218f731a9a5fc3924b0d29a9a1773
Ryan Goderis <rgoderis789@gmail.com>
2020-02-10 13:05:31 -0500

    added contact to state of NewContact that is an object which contains a value for each input field

--- client/src/Components/NewContact/index.js
+++ client/src/Components/NewContact/index.js
@@ -1,10 +1,23 @@
 import React from "react";
+import API from "../../utils/API"
 
 class NewContact extends React.Component{
     state = {
         months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
         days:[],
-        states: ["AL","AK", "AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA", "WA","WV","WI","WY"]
+        states: ["AL","AK", "AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA", "WA","WV","WI","WY"],
+        contact: {
+            firstName: "",
+            lastName: "",
+            email: "",
+            phoneNumber: "",
+            birthMonth: "",
+            birthDay: "",
+            address: "",
+            city: "",
+            state: "",
+            zip: ""
+        }
     }
 
     componentDidMount(){
